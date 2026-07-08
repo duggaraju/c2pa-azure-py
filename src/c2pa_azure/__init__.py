907a0e65a94be3bf11236fa484cdc875e95fecf4
Prakash Duggaraju <duggaraju@gmail.com>
2026-07-08 12:30:17 -0700

    Upgrade to c2pa-python 0.36.0

--- src/c2pa_azure/__init__.py
+++ src/c2pa_azure/__init__.py
@@ -3,4 +3,4 @@ from .signer import AzureSigner
 from .trusted_signing import TrustedSigningClient, TrustedSigningSettings
 
 __all__ = ["AzureSigner", "TrustedSigningClient", "TrustedSigningSettings"]
-__version__ = "0.34.0.1"
+__version__ = "0.36.0"
